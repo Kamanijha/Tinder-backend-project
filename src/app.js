60071365dbdcc7ff16636815bc53426c23736057
kamani jha <kamanijha10@gmail.com>
2025-02-23 09:07:40 +0530

    different type of route

--- src/app.js
+++ src/app.js
@@ -2,23 +2,26 @@ const express = require("express")
 
 const app = express()
 
-app.use("/",(req,res)=>{
-    res.send('hello hello hello')
+app.use("/",(req,res,next)=>{
+    //res.send("handling / route")
+    next()
 })
 
-app.use("/jha",(req,res)=>{
-    res.send('hello hello hello')
-})
+app.get(
+"/user",
+(req,res,next)=>{
+    console.log("handling the route user1")
+    next()
+    //res.send("router handler")
+},(req,res,next)=>{
+    console.log("handling the route user2")
+    res.send("router handler 2nd")
+    
+}
 
-app.use("/test",(req,res)=>{
-    res.send('test test test')
-})
+)
 
-app.use((req,res)=>{
-    res.send('hello from server')
-})
 app.listen(7777,()=>{
     console.log("start server on 7777")
 })
 
-//console.log(app)
