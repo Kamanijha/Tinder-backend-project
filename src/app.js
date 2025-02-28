5ed35bba37af853903852026e09095a15a0ab463
kamani jha <kamanijha10@gmail.com>
2025-02-28 12:21:58 +0530

    receive the data from the end user and send into database

--- src/app.js
+++ src/app.js
@@ -2,15 +2,36 @@ const express = require("express")
 const connectDB = require("./config/database")
 const app = express()
 const User = require("./models/user")
+// express middleware 
+app.use(express.json())
 
+// this code is for understanding
 
-app.post('/signup',async (req,res)=>{
-    const user = new User({
-        firstName :"jone",
-        lastName:"doe",
-        emailId :"jone@gmail.com",
-        password:"jone@123"
-    })
+// app.post('/signup', async (req,res)=>{
+//     console.log(req.body)
+//     res.send("sucess")
+//     // creating a new instance of the User model
+//     const user = new User({
+//         firstName :"suman",
+//         lastName:"doe",
+//         emailId :"suman@gmail.com",
+//         password:"suman@123",
+//         age:"29",
+//         gender:"male"
+//     })
+//     try{
+//         await user.save()
+//         res.send("User addd successfully")
+//     }catch(err){
+//         res.status(400).send("error saving the users" + err.message)
+//     }
+// })
+
+app.post('/signup', async (req,res)=>{
+    console.log(req.body)
+    
+    // creating a new instance of the User model
+    const user = new User(req.body)
     try{
         await user.save()
         res.send("User addd successfully")
@@ -18,8 +39,6 @@ app.post('/signup',async (req,res)=>{
         res.status(400).send("error saving the users" + err.message)
     }
 })
-
-
 
 // remmeber first connect your database and then app.listen
 connectDB().then(() =>{
